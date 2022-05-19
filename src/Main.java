ec42e8509080956a078bc9b68f3b71119f7819cd
Mike Alexandrov <m.s.alexandroff@yandex.ru>
2022-05-19 18:24:34 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -3,52 +3,57 @@ import java.util.*;
 public class Main {
 
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         String regex = "-?\\d+(\\.\\d+)?";
         int result;
+        int a;
+        int b;
         Scanner scanner = new Scanner(System.in);
         String[] array = scanner.nextLine().split(" ");
-        if (array.length > 3) {
-            System.out.println("Ошибка! Некорректно введен пример");
-            return;
+        if (array.length != 3) {
+            throw new Exception("Неверно написан пример");
         }
         if ((array[0].matches(regex) && array[2].matches(regex)) || (!array[0].matches(regex) && !array[2].matches(regex))) {
             if (!array[0].matches(regex)) {
-               result =  doMath(romanToArabic(array[0]), romanToArabic(array[2]), array[1]);
-               if (result == -101) return;
-               if (result < 0) {
-                   System.out.println("Ошибка! В римской системе нет отрицательных чисел");
-                   return;
-               }
+                a = romanToArabic(array[0]);
+                b = romanToArabic(array[2]);
+                if(a < 0 || a > 10 || b < 0 || b > 10) {
+                    throw new Exception("Ошибка! Одно из чисел вне диапазона от 0 до 10");
+                }
+                result = doMath(a, b, array[1]);
+                if (result < 0) {
+                    throw new Exception("Ошибка! В римской системе нет отрицательных чисел");
+                }
                 System.out.println(arabicToRoman(result));
             } else {
-                result = doMath(Integer.parseInt(array[0]), Integer.parseInt(array[2]), array[1]);
-                if (result == -101) return;
+                a = Integer.parseInt(array[0]);
+                b = Integer.parseInt(array[2]);
+                if(a < 0 || a > 10 || b < 0 || b > 10) {
+                    throw new Exception("Ошибка! Одна из чисел вне диапазона от 0 до 10");
+                }
+                result = doMath(a, b, array[1]);
                 System.out.println(result);
             }
         } else {
-            System.out.println("Ошибка! Не совпадают системы чисел");
-            return;
+            throw new Exception("Ошибка! Не совпадают системы счисления");
         }
 
 
     }
 
-    public static int doMath(int n1, int n2, String operator) {
-       switch (operator){
-           case "+":
-               return n1 + n2;
-           case "-":
-               return n1 - n2;
-           case "*":
-               return n1 * n2;
-           case "/":
-               return n1 / n2;
-           default:
-               System.out.println("Ошибка! неизвестный оператор");
-               return -101;
+    public static int doMath(int n1, int n2, String operator) throws Exception {
+        switch (operator) {
+            case "+":
+                return n1 + n2;
+            case "-":
+                return n1 - n2;
+            case "*":
+                return n1 * n2;
+            case "/":
+                return n1 / n2;
+            default:
+                throw new Exception("Ошибка! неизвестный оператор");
         }
     }
 
@@ -71,15 +76,15 @@ public class Main {
         }
 
         if (romanNumber.length() > 0) {
-            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+            throw new IllegalArgumentException(input + " нельзя перевести в арабскую систему счисления");
         }
 
         return result;
     }
 
     public static String arabicToRoman(int number) {
-        if ((number <= 0) || (number > 4000)) {
-            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        if ((number <= 0) || (number > 100)) {
+            throw new IllegalArgumentException(number + " не в диапазоне от 0 до 100");
         }
 
         List<RomanNumbers> romanNumbers = RomanNumbers.getReverseSortedValues();
